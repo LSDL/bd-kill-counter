9709626e600d511fb8bba43f9fe80b8d70dbbc26
LSDL <kkho9950@gmail.com>
2018-04-08 19:10:53 +0900

    v1.03

--- kill-count.js
+++ kill-count.js
@@ -1,7 +1,8 @@
 var h1 = document.getElementsByTagName('h1')[0],
     table = document.getElementsByTagName('table')[0],
-    start = document.getElementById('start'),
-    record = document.getElementById('record'),
+    btnStart = document.getElementById('start'),
+    btnReset = document.getElementById('reset'),
+    btnRecord = document.getElementById('record'),
     killStart = document.getElementById('kill-start'),
     kill = document.getElementById('kill'),
     t;
@@ -23,33 +24,51 @@ function getTimestamp(seconds) {
   return (h ? (h > 9 ? h : "0" + h) : "00") + ":" + (m ? (m > 9 ? m : "0" + m) : "00") + ":" + (s > 9 ? s : "0" + s);
 }
 
-start.onclick = function() {
-  if (start.innerHTML == "중단") {
-    start.innerHTML = "재개";
-    start.classList.remove("btn-danger");
-    start.classList.add("btn-success");
-    record.setAttribute("disabled", "disabled");
+btnStart.onclick = function() {
+  if (btnStart.innerHTML == "중단") {
+    btnStart.innerHTML = "재개";
+    btnStart.classList.remove("btn-danger");
+    btnStart.classList.add("btn-success");
+    btnRecord.setAttribute("disabled", "disabled");
     killStart.removeAttribute("disabled");
     clearTimeout(t);
 
   } else {
-    start.innerHTML = "중단";
-    start.classList.remove("btn-success");
-    start.classList.add("btn-danger");
+    btnStart.innerHTML = "중단";
+    btnStart.classList.remove("btn-success");
+    btnStart.classList.add("btn-danger");
     var c = parseInt(killStart.value);
     if (c < 0) {
       alert("0 이상의 숫자를 입력해주세요!");
       return
     }
     killStart.setAttribute("disabled", "disabled");
-    record.removeAttribute("disabled");
+    btnRecord.removeAttribute("disabled");
     cnt = c;
     tStart = Date.now() / 1000 | 0;
     t = setTimeout(timer, 200);
+    kill.focus();
   }
 };
 
-record.onclick = function() {
+btnReset.onclick = function() {
+  if (btnStart.innerHTML != "시작") {
+    btnStart.innerHTML = "시작";
+    btnStart.classList.remove("btn-danger");
+    btnStart.classList.add("btn-success");
+    btnRecord.setAttribute("disabled", "disabled");
+    killStart.removeAttribute("disabled");
+    clearTimeout(t);
+
+    h1.textContent = "00:00:00"
+    kill.value = "";
+    document.getElementById("table-body").innerHTML = "";
+    last = tStart = 0;
+    killStart.focus();
+  }
+};
+
+btnRecord.onclick = function() {
   var now = (Date.now() / 1000 | 0) - tStart;
   var r = now - last;
   var d = kill.value;
@@ -59,7 +78,8 @@ record.onclick = function() {
   }
   var dt = d - cnt;
 
-  var row = table.insertRow(table.rows.length);
+  var tbody = table.getElementsByTagName('tbody')[0];
+  var row = tbody.insertRow(tbody.rows.length);
   var cell0 = row.insertCell(0);
   var cell1 = row.insertCell(1);
   var cell2 = row.insertCell(2);
@@ -70,7 +90,6 @@ record.onclick = function() {
   cell3.innerHTML = Math.round(dt/(r/60)*100)/100;
 
   cnt = d;
-  last = now;
   kill.select();
 };
 
@@ -85,7 +104,7 @@ kill.onfocus = function() {
 killStart.addEventListener("keyup", function(event) {
   event.preventDefault();
   if (event.keyCode === 13) {
-    start.click();
+    btnStart.click();
     kill.focus();
   }
 }); 
@@ -93,6 +112,6 @@ killStart.addEventListener("keyup", function(event) {
 kill.addEventListener("keyup", function(event) {
   event.preventDefault();
   if (event.keyCode === 13) {
-    record.click();
+    btnRecord.click();
   }
 }); 
